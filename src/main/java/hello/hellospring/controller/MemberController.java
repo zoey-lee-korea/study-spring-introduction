a22c26fd6127b1dc6b9bb5be5fd26d2807fe0eaf
zoey-lee-korea <wngpsid4014@gmail.com>
2021-05-02 22:45:07 +0900

    home,회원가입,회원목록 HTML 화면 추가

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(createForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
+}
